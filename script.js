a70698490ca3eac673dcde23d36e1ba7f60bdb19
Fozil <fozil@gmail.com>
2022-03-26 14:33:41 +0500

    js

---
+++ script.js
@@ -0,0 +1,42 @@
+let yourName = prompt('15*2/3+45-12')
+let misol = 15*2/3+45-12
+let javob = 'sizning javobingiz '
+console.log(javob + yourName);
+let javobb = 'togri javob '
+console.log(javobb + misol)
+let Yourname = prompt('15+5-10*24')
+let Misol = 15+5-10*24
+let Javob = 'sizning javobingiz '
+console.log(javob + Yourname);
+let Javobb = 'togri javob '
+console.log(javobb + Misol)
+let yourname = prompt('15*2/3+45-12')
+let mIsol = 15*2/3+45-12
+let jAvob = 'sizning javobingiz '
+console.log(jAvob + yourname);
+let jAvobb = 'togri javob '
+console.log(jAvobb + mIsol)
+let yOurame = prompt('80-20+10*2/4')
+let miSol = 80-20+10*2/4
+let jaVob = 'sizning javobingiz '
+console.log(jaVob + yOurame);
+let jaVobb = 'togri javob '
+console.log(jaVobb + miSol)
+let youRName = prompt('90/3+15*2-4')
+let misOl = 90/3+15*2-4
+let javOb = 'sizning javobingiz '
+console.log(javOb + youRName);
+let javObb = 'togri javob '
+console.log(javObb + misOl)
+let yourNAme = prompt('70-30/2*3+14')
+let misoL = 70-30/2*3+14
+let javoB = 'sizning javobingiz '
+console.log(javoB + yourNAme);
+let javoBb = 'togri javob '
+console.log(javoBb + misoL)
+let yourNamE = prompt('45*2/3+30-12')
+let misoll = 45*2/3+30-12
+let jaavob = 'sizning javobingiz '
+console.log(jaavob + yourNamE);
+let javobB = 'togri javob '
+console.log(javobB + misoll)
